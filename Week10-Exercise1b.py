c993cd4a9ac8a2777ce0fbe87d3d80cfa1e54a23
theone333 <32265996+theone333@users.noreply.github.com>
2017-12-05 20:28:03 +0000

    Update Week10-Exercise1b.py

--- Week10-Exercise1b.py
+++ Week10-Exercise1b.py
@@ -1,50 +1,7 @@
-class Student():
+from Student import Student
 
-    def __init__ (self, name, surname, studentnumber, course):
-        
-        self.name = name
-        self.surname = surname
-        self.studentnumber = studentnumber
-        self.course = course
+harry = Student("Harold", "Jones", "21325641", "Mathematics")
+pete = Student("Peter", "Ball", "26754352", "Psychology")
 
-    def printinfo(self):
-        
-        print("My name is "+self.name)
-        print("My surname is "+self.surname)
-        print("My student number is "+self.studentnumber)
-        print("My course is "+self.course)
-
-    def updateCourse(self,myNewCourse):
-        self.location = myNewCourse
-        
-
-    def getName(self):
-        return self.name
-
-    def getSurname(self):
-        return self.surname
-
-
-    def getStudentnumber(self):
-        return self.studentnumber
-
-
-    def getCourse(self):
-        return self.course
-
-
-                
-    def setName(self, nameinput):
-        self.name = nameinput   
-
-     
-    def setSurame(self, surnameinput):
-        self.surname = surnameinput   
-
-        
-    def setStudentnumber(self, studentnumberinput):
-        self.studentnumber = studentnumberinput  
-               
-        
-    def setCourse(self, courseinput):
-        self.course = courseinput
+harry.printinfo()   
+pete.printinfo()
