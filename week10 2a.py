5a04d7ae3dc8e822ddc4099c0ab024e7e8775633
theone333 <32265996+theone333@users.noreply.github.com>
2017-12-05 20:27:14 +0000

    Add files via upload

---
+++ week10 2a.py
@@ -0,0 +1,22 @@
+
+class CashRegister :
+   
+   def __init__(self) :
+      self._itemCount = 0
+      self._totalPrice = 0.0
+      
+   
+   def addItem(self, price) :
+      self._itemCount = self._itemCount + 1
+      self._totalPrice = self._totalPrice + price 
+      
+   
+   def getTotal(self) :
+      return self._totalPrice
+
+   def getCount(self) :
+      return self._itemCount
+
+   def clear(self) :
+      self._itemCount = 0
+      self._totalPrice = 0.0
